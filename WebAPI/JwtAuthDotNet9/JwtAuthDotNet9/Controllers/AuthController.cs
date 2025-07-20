d3ecf4940f2c01f16680b88c05b8d6235ca7c859
Gyanendra Chaudhary <gyanendra.chaudhary@avishkaram.com>
2025-07-21 01:52:26 +0545

    successfully implemented refresh token logic in this project

---
+++ WebAPI/JwtAuthDotNet9/JwtAuthDotNet9/Controllers/AuthController.cs
@@ -0,0 +1,61 @@
+﻿using JwtAuthDotNet9.Entities;
+using JwtAuthDotNet9.Models;
+using JwtAuthDotNet9.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace JwtAuthDotNet9.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController(IAuthService _authService) : ControllerBase
+    {
+
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register(UserDto request)
+        {
+            var user = await _authService.RegisterAsync(request);
+            if (user is null)
+            {
+                return BadRequest("User already exists!");
+            }
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<TokenResponseDto>> Login(UserDto request)
+        {
+            var result = await _authService.LoginAsync(request);
+            if (result is null)
+            {
+                return BadRequest("Invalid username or password!");
+            }
+            return Ok(result);
+        }
+
+        [HttpPost("refresh-token")]
+        public async Task<ActionResult<TokenResponseDto>> RefreshToken(RefreshTokenRequestDto request)
+        {
+            var result = await _authService.RefreshTokenAsync(request);
+            if (result is null || result.AccessToken is null || result.RefreshToken is null)
+            {
+                return Unauthorized("Invalid refresh token!");
+            }
+            return Ok(result);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult AuthenticatedEndpoint()
+        {
+            return Ok("Your are authenticated");
+        }
+        [Authorize(Roles = "Admin")]
+        [HttpGet("admin-only")]
+        public IActionResult AdminOnlyEndpoint()
+        {
+            return Ok("Your are an admin!");
+        }
+    }
+}
---
+++ WebAPI/JwtAuthDotNet9/JwtAuthDotNet9/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using JwtAuthDotNet9.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace JwtAuthDotNet9.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<User> Users { get; set; }
+
+    }
+}
